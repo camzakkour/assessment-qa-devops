e78ec89b443e781673eebd59cd387bc3690c9a14
camzakkour <camzakkour@gmail.com>
2022-02-25 11:14:43 -0800

    fixed server issue

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-app.get('/api/robots', (req, res) => {
+app.get('http://localhost:3000/api/robots', (req, res) => {
     try {
         res.status(200).send(botsArr)
     } catch (error) {
@@ -32,7 +32,7 @@ app.get('/api/robots', (req, res) => {
     }
 })
 
-app.get('/api/robots/five', (req, res) => {
+app.get('http://localhost:3000/api/robots/five', (req, res) => {
     try {
         let shuffled = shuffleArray(bots)
         let choices = shuffled.slice(0, 5)
@@ -44,7 +44,7 @@ app.get('/api/robots/five', (req, res) => {
     }
 })
 
-app.post('/api/duel', (req, res) => {
+app.post('http://localhost:3000/api/duel', (req, res) => {
     try {
         // getting the duos from the front end
         let {compDuo, playerDuo} = req.body
@@ -75,7 +75,7 @@ app.post('/api/duel', (req, res) => {
     }
 })
 
-app.get('/api/player', (req, res) => {
+app.get('http://localhost:3000/api/player', (req, res) => {
     try {
         res.status(200).send(playerRecord)
     } catch (error) {
